cab0d026f8dd83793cc75ca9d88ec9fbc614da2e
JDHey <jayden.leslie@gmail.com>
2021-03-09 01:39:39 +1030

    Updated calculation to work with s and z

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 function calculate() {
-    alphabetNums = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqr', 'tuv', 'wxy']
+    alphabetNums = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
     number = document.getElementById("textNumbers").value;
     possiblePerms = []
     for (var i = 0; i < number.length; i++) {
         ///alert(alphabetNums[number.charAt(i)-2]);
         possiblePerms.push(alphabetNums[number.charAt(i)-2])
     }
-    getAllPermutations(possiblePerms);
+    //getAllPermutations(possiblePerms);
+    document.getElementById('output').innerHTML = ''
+    graph('',possiblePerms)
 }
 
+
+function graph(charList, arrayOfSets) {
+    if (arrayOfSets.length == 0) {
+        var listItem = document.createElement("li");
+        var listItemText = document.createTextNode(charList);
+        listItem.appendChild(listItemText);
+        document.getElementById('output').appendChild(listItem);
+        console.log(charList)
+        return null
+    } else {
+        for (var char = 0; char < arrayOfSets[0].length; char++) {
+            graph(charList + arrayOfSets[0].charAt(char), arrayOfSets.slice(1))
+        }
+    }
+}
+/*
 function getAllPermutations(arrayOfSets) {
-    possiblePermutations = 3**arrayOfSets.length
+    possiblePermutations = getAllCharacterLength(arrayOfSets)*arrayOfSets.length
     perm = ''
     for (var i = 0; i < possiblePermutations; i++) {
         perm = i.toString(3)
@@ -31,5 +49,31 @@ function getAllPermutations(arrayOfSets) {
         listItem.appendChild(listItemText);
         document.getElementById('output').appendChild(listItem);
     }
-
 }
+
+function getAllCharacterLength(arrayOfSets) {
+    count = 0;
+    for (var eachSet = 0; eachSet < arrayOfSets.length; eachSet++) {
+        count += arrayOfSets[eachSet].length
+    }
+    return count
+} */
+
+
+/*
+    create a queue Q
+    enqueue v onto Q
+    mark v
+    while Q is not empty do
+        w ← Q.dequeue()
+        if w is what we are looking for then
+            return w
+        for all edges e in G.adjacentEdges(w) do
+            x ← G.adjacentVertex(w, e)
+            if x is not marked then
+                mark x
+                enqueue x onto Q
+    return null
+
+               678 = [MNO, PQRS, TUV] 
+               */
